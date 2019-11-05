aa2d86aa40284518686d5a504ab532520c1c571e
Patrik Jonell <pjjonell@kth.se>
2019-11-05 16:36:13 +0100

    added default port and some cleanup

--- zmq_handler/fe_zmq_server/__init__.py
+++ zmq_handler/fe_zmq_server/__init__.py
@@ -5,16 +5,18 @@ import msgpack
 import sys
 import traceback
 
+
 def _construct_msg(msg, success, files=None):
     out = {"success": success, "msg": msg}
     if files:
         out["files"] = files
     return msgpack.dumps(out, use_bin_type=True)
 
-def listen(cli):
+
+def listen(cli, port=5555):
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:5555")
+    socket.bind("tcp://*:{}".format(port))
 
     while True:
         #  Wait for next request from client
@@ -75,26 +77,20 @@ def listen(cli):
                 socket.send(_construct_msg(e.format_message(), False))
             except Exception as e:
                 ex_type, ex_value, ex_traceback = sys.exc_info()
-
-                # Extract unformatter stack traces as tuples
                 trace_back = traceback.extract_tb(ex_traceback)
-
-                # Format stacktrace
-                stack_trace = list()
-
+                stack_trace = []
                 for trace in trace_back:
                     stack_trace.append(
-                        "File : %s , Line : %d, Func.Name : %s, Message : %s"
-                        % (trace[0], trace[1], trace[2], trace[3])
-                    )
-
-                socket.send(
-                    _construct_msg(
                         {
-                            "stack_trace": stack_trace,
-                            "exception_type": ex_type.__name__,
-                            "exception_msg": str(ex_value),
-                        },
-                        False,
+                            "file": trace[0],
+                            "line": trace[1],
+                            "func_name": trace[2],
+                            "message": trace[3],
+                        }
                     )
-                )
+                error_msg = {
+                    "stack_trace": stack_trace,
+                    "exception_type": ex_type.__name__,
+                    "exception_msg": str(ex_value),
+                }
+                socket.send(_construct_msg(error_msg, False,))
