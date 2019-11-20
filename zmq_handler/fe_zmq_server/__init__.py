6433d5304f9b1f2aa7372ce89f0f766748b08645
Patrik Jonell <pjjonell@kth.se>
2019-11-20 10:29:36 +0100

    updates to zmq server and ringnet extractor

--- zmq_handler/fe_zmq_server/__init__.py
+++ zmq_handler/fe_zmq_server/__init__.py
@@ -5,6 +5,13 @@ import msgpack
 import sys
 import traceback
 import click
+import tarfile
+import os
+
+try:
+    from cStringIO import StringIO as BIO
+except ImportError:  # python 3
+    from io import BytesIO as BIO
 
 
 def _construct_msg(msg, success, files=None):
@@ -51,7 +58,24 @@ def listen(cli, port=5555):
                         tmpf.close()
 
                     if value["type"] == "output_file":
-                        output_files[key] = tmpf.name
+                        output_files[key] = {
+                            "name": tmpf.name,
+                            "type": "file",
+                            "compression": value.get("compression"),
+                        }
+                    elif value["type"] == "output_dir":
+                        with backports.tempfile.TemporaryDirectory(dir=tmpd) as tmpd2:
+                            tmpd2_name = tmpd2
+                        tmp_tar = tempfile.NamedTemporaryFile(
+                            suffix=".tar", dir=tmpd, delete=False
+                        )
+                        output_files[key] = {
+                            "name": tmpd2_name,
+                            "tar": tmp_tar,
+                            "type": "dir",
+                            "compression": value.get("compression"),
+                        }
+                        argument_value = tmpd2
                 else:
                     argument_value = value
 
@@ -69,9 +93,20 @@ def listen(cli, port=5555):
                 result = command(arguments, standalone_mode=False)
 
                 files = {}
-                for key, filename in output_files.items():
-                    with open(filename, "rb") as f:
-                        files[key] = f.read()
+                for key, out_file in output_files.items():
+                    if out_file["type"] == "file":
+                        with open(out_file["name"], "rb") as f:
+                            files[key] = f.read()
+                    elif out_file["type"] == "dir":
+                        file_out = BIO()
+                        with tarfile.open(mode="w", fileobj=file_out) as tar:
+                            tar.add(
+                                out_file["name"],
+                                arcname=os.path.basename(out_file["name"]),
+                            )
+                        file_out.seek(0)
+                        files[key] = file_out.read()
+
                 socket.send(_construct_msg(result, True, files=files))
             except click.exceptions.MissingParameter as e:
                 socket.send(_construct_msg(e.format_message(), False))
